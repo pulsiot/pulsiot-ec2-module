c275324e7fddb8329e60ce5edda9d67252966b30
debuggerboy <debuggerboy@gmail.com>
2022-05-07 05:42:46 +0530

    updates

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "pulsiot_ec2_instance" {
   ami           = var.ami_id
   instance_type = var.inst_type
   key_name = var.key_pair
-  security_groups = var.security_groups
+  security_groups = [ var.security_groups ]
 
   tags = {
     Name = var.inst_name,
